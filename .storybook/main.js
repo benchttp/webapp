d4077f0e17061a93d3f027435c748bbd17bff3f6
alex-mngt <mongeot.alex@gmail.com>
2022-02-02 16:21:43 +0100

    chore : update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  previewHead: (head) => `
+  ${head}
+    <style>
+      body {
+        background: #26282E;
+      }
+    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Poppins:wght@700&display=swap" rel="stylesheet">
+  `,
 }
