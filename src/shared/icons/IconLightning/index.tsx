5b7f1992de98a73419557a7b748ac3a3019694c3
alex-mngt <mongeot.alex@gmail.com>
2022-03-01 00:43:53 +0100

    chore : improv svg component

--- src/shared/icons/IconLightning/index.tsx
+++ src/shared/icons/IconLightning/index.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react'
 import { ISVGProps } from 'shared/helpers/types'
 import { COLORS } from 'shared/style/constants'
 
-export const IconSearch: FC<ISVGProps> = ({
+export const IconLightning: FC<ISVGProps> = ({
   color,
   className,
   width = 24,
@@ -11,7 +11,7 @@ export const IconSearch: FC<ISVGProps> = ({
 }) => {
   return (
     <svg
-      className={className || ''}
+      className={`${className || ''}`}
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
@@ -22,8 +22,7 @@ export const IconSearch: FC<ISVGProps> = ({
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <circle cx="11" cy="11" r="8"></circle>
-      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+      <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
     </svg>
   )
 }
