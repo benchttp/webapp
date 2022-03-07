c2879d2449f803aa1a940aa8605ada2f4074e2c8
Damien-MATHIEU <damien.mathieu612@gmail.com>
2022-03-07 18:03:24 +0100

    Fix: component className props

--- src/components/PrimaryButton/index.tsx
+++ src/components/PrimaryButton/index.tsx
@@ -4,7 +4,11 @@ import { StyledPrimaryButton } from './core/primaryButton.styles'
 import { IProps } from './core/primaryButton.types'
 
 export const PrimaryButton: FC<IProps> = (props) => {
-  const { children } = props
+  const { children, className } = props
 
-  return <StyledPrimaryButton {...props}>{children}</StyledPrimaryButton>
+  return (
+    <StyledPrimaryButton className={`${className || ''}`}>
+      {children}
+    </StyledPrimaryButton>
+  )
 }
