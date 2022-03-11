8e135cbb3cdcb44cca934e89df3acb9472a6a47c
Damien-MATHIEU <damien.mathieu612@gmail.com>
2022-03-11 13:33:50 +0100

    chore: make headers dynamic with the api call

--- src/components/Cards/CardConfig/components/CardConfigContent/index.tsx
+++ src/components/Cards/CardConfig/components/CardConfigContent/index.tsx
@@ -1,30 +1,33 @@
-import { Text } from 'components/Text'
 import { FC } from 'react'
+import { Text } from 'components/Text'
 
 import { StyledCardConfigContent } from './core/cardConfigContent.styles'
 import { FontFamilies, FontSizes, FontWeights } from 'shared/style/constants'
 
-export const CardConfigContent: FC = () => {
-  const headerContent = Object.entries({
-    key0: 'value0',
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-    key4: 'value4',
-  })
+import { useGetReportsByIdQuery } from 'shared/store/apis'
+import { useAppSelector } from 'shared/store'
+import { skipToken } from '@reduxjs/toolkit/dist/query'
 
-  const headersList = headerContent.map(([key, value], index) => {
-    return (
-      <tr key={index} className="f keyValueCouple">
-        <td className="keyColumn">
-          <Text>{key}</Text>
-        </td>
-        <td className="valueColumn">
-          <Text>{value}</Text>
-        </td>
-      </tr>
-    )
-  })
+export const CardConfigContent: FC = () => {
+  const selectedRunId = useAppSelector((state) => state.general.selectedRunId)
+  const { data: report } = useGetReportsByIdQuery(selectedRunId ?? skipToken)
+
+  const headersList = report
+    ? Object.entries(report.metadata.config.request.header).map(
+        ([key, value], index) => {
+          return (
+            <tr key={index} className="f keyValueCouple">
+              <td className="keyColumn">
+                <Text>{key}</Text>
+              </td>
+              <td className="valueColumn">
+                <Text>{value.join()}</Text>
+              </td>
+            </tr>
+          )
+        }
+      )
+    : null
 
   return (
     <StyledCardConfigContent className="mt-4 mr-2 ml-2 mb-2 p-3 br-1">
@@ -37,15 +40,19 @@ export const CardConfigContent: FC = () => {
         >
           Headers
         </Text>
-        <table className="f f-jc-space-a headersListContainer">
-          <thead>
-            <tr className="f keyValueCouple">
-              <th className="keyColumn">Key</th>
-              <th className="valueColumn">Value</th>
-            </tr>
-          </thead>
-          <tbody>{headersList}</tbody>
-        </table>
+        {headersList && headersList.length !== 0 ? (
+          <table className="f f-jc-space-a headersListContainer">
+            <thead>
+              <tr className="f keyValueCouple">
+                <th className="keyColumn">Key</th>
+                <th className="valueColumn">Value</th>
+              </tr>
+            </thead>
+            <tbody>{headersList}</tbody>
+          </table>
+        ) : (
+          <Text>There are no headers.</Text>
+        )}
       </div>
     </StyledCardConfigContent>
   )
